b90c384a672ad5a2dd7d979ea949299de3f2f857
Raven Demeyer (imec) <demeye78@imec.be>
2016-07-13 11:17:15 +0200

    example issue

--- src/app.ts
+++ src/app.ts
@@ -1,81 +1,9 @@
-/// <reference path="../typings/globals/jquery/index.d.ts" />
-/// <reference path="../typings/globals/angular/index.d.ts" />
-/// <reference path="../typings/globals/angular-material/index.d.ts"/>
-/// <reference path="../typings/globals/angular-ui-router/index.d.ts"/>
+// --- FILE main.ts ---
+/// <reference path="../typings/index.d.ts" />
 
 module app {
-    'use strict';
-    console.log('app.ts ... started!');
-    angular.module('digiangularjs', [
-      'ui.router',
-      'ngMaterial',
-      'ngMdIcons',
-      'digiangularjs.home',
-      'digiangularjs.page1',
-      'digiangularjs.page2',
-    ])
-      .config(config)
-    ;
-
-    config.$inject = [
-        '$locationProvider',
-        '$stateProvider',
-        '$urlRouterProvider',
-        '$mdThemingProvider'
-    ];
-
-    function config($locationProvider: ng.ILocationProvider,
-        $stateProvider: angular.ui.IStateProvider,
-        $urlRouterProvider: angular.ui.IUrlRouterProvider,
-        $mdThemingProvider: angular.material.IThemingProvider
-      ) {
-        //change default color for primary/*
-       var indigo = $mdThemingProvider.extendPalette('indigo', {
-           '500': '569fd4'
-       });
-       $mdThemingProvider.definePalette('indigo', indigo);
-
-       //change default color for warn
-       indigo = $mdThemingProvider.extendPalette('red', {
-           '500': 'ff5800'
-       });
-       $mdThemingProvider.definePalette('red', indigo);
-
-       $mdThemingProvider.theme('default').primaryPalette('indigo').warnPalette('red');
-
-       //here you change placeholder/foreground color.
-       $mdThemingProvider.theme('default').foregroundPalette[3] = "rgba(0,0,0,0.67)";
-
-
-        //html5 removes the need for # in URL
-        $locationProvider.html5Mode({
-            enabled: false,
-            requireBase: false
-        });
-
-        $urlRouterProvider.otherwise('/');
-
-        //angular-ui-router for multiple views
-        $stateProvider
-            .state('home', {
-                url: "/",
-                templateUrl: 'components/home/home.html',
-                controller: 'homeController',
-                controllerAs: 'vm',
-            })
-            .state('home.reports', {
-              url: '/reports/',
-              templateUrl: 'components/reports/reports.html'
-            })
-            .state('home.page1', {
-              url: '/page1/',
-              templateUrl: 'components/page1/page1.html'
-            })
-            .state('home.page2', {
-              url: '/page2/',
-              templateUrl: 'components/page2/page2.html'
-            })
-            ;
-            //more states here.
-    }
+  angular.module('myApp', ["testModule"])
+    .run(() => { console.log('myApp initialized.'); });
 }
+
+// --- FILE main.ts END ---
